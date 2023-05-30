bfe58bb8cd6f0844bde755a492c9f6bcdb4e6473
Felipe Claver Diniz <127131423+FelipeCD61@users.noreply.github.com>
2023-05-30 20:12:08 -0300

    Update VerificacaoCPF.java

--- Lista 7 UNA/ExercicioTres/VerificacaoCPF.java
+++ Lista 7 UNA/ExercicioTres/VerificacaoCPF.java
@@ -1,5 +1,4 @@
-import java.util.Scanner;
-import java.util.Arrays;
+import java.util.Scanner;
 public class VerificacaoCPF {
     public static void main(String []args) {
         Scanner scanner = new Scanner(System.in);
