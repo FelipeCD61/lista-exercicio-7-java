a31e661a98599294c808df63c2f547a1cf305a93
Felipe Claver Diniz <127131423+FelipeCD61@users.noreply.github.com>
2023-06-01 11:00:17 -0300

    Update VerificacaoCPF.java

--- Lista 7 UNA/ExercicioTres/VerificacaoCPF.java
+++ Lista 7 UNA/ExercicioTres/VerificacaoCPF.java
@@ -1,6 +1,8 @@
-import java.util.Scanner;
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class VerificacaoCPF {
-    public static void main(String []args) {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         SistemaVerificacaoCPF sistemaVerificacaoCPF = new SistemaVerificacaoCPF();
 
@@ -9,19 +11,8 @@ public class VerificacaoCPF {
         System.out.print("Digite seu CPF(xxx.xxx.xxx-xx): ");
         sistemaVerificacaoCPF.setCpf(scanner.next());
 
-        
-        if (sistemaVerificacaoCPF.getCpf().length() == 14) {
-            if (sistemaVerificacaoCPF.getCpf().charAt(3) == '.' && sistemaVerificacaoCPF.getCpf().charAt(7) == '.' && sistemaVerificacaoCPF.getCpf().charAt(11) == '-') {
-                System.out.println("\nCPF válido");
-            } else {
-                System.out.println("CPF inválido.");
-            }
-        } else {
-            System.out.println("CPF inválido.");
-        }
-               
-               
-        
+        sistemaVerificacaoCPF.validaCPF();
+
         scanner.close();
     }
 }
